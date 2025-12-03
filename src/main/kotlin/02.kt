c23c2cc254f68467591536ccb906a1972a90b483
BTheofil <btheofil7@gmail.com>
2025-12-03 17:55:55 +0100

    d2 p1

---
+++ src/main/kotlin/02.kt
@@ -0,0 +1,39 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+fun main() {
+    day2part1()
+}
+
+fun day2part1() {
+    val invalidIds = mutableListOf<Long>()
+    //val mock = listOf(446446,38593859)
+    parseInput2().forEach { (start, end) ->
+        for (id in start..end) {
+            val idLength = id.toString().length
+            if (idLength % 2 == 0) {
+                var matched = 0
+                for (i in 0..<idLength / 2) {
+                    if (id.toString()[i] == id.toString()[(idLength / 2) + i]) {
+                        matched++
+                    }
+                    if (matched == idLength / 2) {
+                        invalidIds.add(id)
+                    }
+                }
+            }
+        }
+    }
+
+    println(invalidIds.sum())
+}
+
+fun parseInput2(): List<Pair<Long, Long>> =
+    Path("src/main/resources/day2.txt")
+        .readLines()
+        .flatMap { line ->
+            line.split(",").map { range ->
+                val (start, end) = range.split("-")
+                start.toLong() to end.toLong()
+            }
+        }
