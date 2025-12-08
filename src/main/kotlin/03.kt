dc94de3bcc5be6fb9f1043dfb4e493d2b37417ed
BTheofil <btheofil7@gmail.com>
2025-12-08 17:00:26 +0100

    d3 p2

--- src/main/kotlin/03.kt
+++ src/main/kotlin/03.kt
@@ -2,7 +2,8 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 fun main() {
-    day3part1()
+    //day3part1()
+    day3part2()
 }
 
 private fun day3part1() {
@@ -26,6 +27,45 @@ private fun day3part1() {
     }
 
     println(banks.sum())
+}
+
+private fun day3part2() {
+    val solutions = mutableListOf<Long>()
+
+    parseFile().forEach { line ->
+        var banks = ""
+        var startIndex = 0
+        (1..12).forEach { _ ->
+            val (nextIndex, digit) = findNextBigDigit(
+                startIndex = startIndex,
+                line = line,
+                banksLength = banks.length
+            )
+            startIndex = nextIndex + 1
+            banks += digit
+        }
+        solutions.add(banks.toLong())
+    }
+
+    println(solutions)
+    println(solutions.sum())
+}
+
+private fun findNextBigDigit(
+    startIndex: Int,
+    line: String,
+    banksLength: Int,
+): Pair<Int, Char> {
+    for (digit in '9' downTo '1') {
+        val indexOfDigit = line.indexOf(
+            startIndex = startIndex,
+            char = digit
+        )
+        if (indexOfDigit != -1 && (line.length - indexOfDigit) >= (12 - banksLength)) {
+            return indexOfDigit to line[indexOfDigit]
+        }
+    }
+    return TODO("Provide the return value")
 }
 
 private fun parseFile(): List<String> = Path("src/main/resources/day3.txt")
