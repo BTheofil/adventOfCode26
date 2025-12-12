f8bb0478e3272839f45eaba733483543c00c8c31
BTheofil <btheofil7@gmail.com>
2025-12-12 21:17:07 +0100

    d4 p1

--- src/main/kotlin/04.kt
+++ src/main/kotlin/04.kt
@@ -2,18 +2,30 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 fun main() {
-    parse()
+    val paperPosList = parse()
+
+    val result = paperPosList.count { pos ->
+        val neighbours = pos.getPosNeighbours()
+        val countExisting = neighbours.count { it in paperPosList }
+        countExisting < 4
+    }
+
+    println(result)
 }
 
-private fun parse(): List<List<Int>> = Path("src/main/resources/day4.txt")
+val neighbourOffsets = listOf(
+    PaperPos(-1, -1), PaperPos(0, -1), PaperPos(1, -1),
+    PaperPos(-1, 0), /*PaperPos(0, 0),*/ PaperPos(1, 0),
+    PaperPos(-1, 1), PaperPos(0, 1), PaperPos(1, 1)
+)
+
+data class PaperPos(val x: Int, val y: Int) {
+    fun getPosNeighbours(): List<PaperPos> =
+        neighbourOffsets.map { PaperPos(x + it.x, y + it.y) }
+}
+
+private fun parse(): List<PaperPos> = Path("src/main/resources/day4.txt")
     .readLines()
-    .map {
-        val temp = mutableListOf<Int>()
-        it.forEach { c ->
-            when (c) {
-                '.' -> temp.add(0)
-                '@' -> temp.add(1)
-            }
-        }
-        temp
+    .flatMapIndexed { y, line ->
+        line.mapIndexedNotNull { x, ch -> if (ch == '@') PaperPos(x = x, y = y) else null }
     }
