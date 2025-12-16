29324d02a88d37953d34c280d90aa4d9d4e09f1d
BTheofil <btheofil7@gmail.com>
2025-12-16 10:10:49 +0100

    d5 p2

--- src/main/kotlin/05.kt
+++ src/main/kotlin/05.kt
@@ -2,14 +2,12 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 fun main() {
+    //part1()
+    part2()
+}
 
-    val ranges = mutableListOf<Pair<String, String>>()
-    for (it in parseFile()) {
-        if (it == "") break
-
-        val (start, end) = it.split("-")
-        ranges.add(start to end)
-    }
+private fun part1() {
+    val ranges = getRanges()
 
     val ids = mutableListOf<String>()
     for (line in parseFile().drop(ranges.size + 1)) {
@@ -28,7 +26,46 @@ fun main() {
 }
 
 private fun part2() {
+    val sortedRanges = getRanges2().sortedBy { it.first }
 
+    var count = 0L
+    var lastId = 0L
+    for (range in sortedRanges) {
+        if (range.first > lastId) {
+            count += (range.last - range.first) + 1
+            lastId = range.last
+        }
+
+        if (range.last > lastId) {
+            val more = range.last - lastId
+            count += more
+            lastId = range.last
+        }
+    }
+
+    println(count)
+}
+
+private fun getRanges2(): List<LongRange> {
+    val ranges = mutableListOf<LongRange>()
+    for (it in parseFile()) {
+        if (it == "") break
+
+        val (start, end) = it.split("-")
+        ranges.add(LongRange(start = start.toLong(), endInclusive = end.toLong()))
+    }
+    return ranges
+}
+
+private fun getRanges(): List<Pair<String, String>> {
+    val ranges = mutableListOf<Pair<String, String>>()
+    for (it in parseFile()) {
+        if (it == "") break
+
+        val (start, end) = it.split("-")
+        ranges.add(start to end)
+    }
+    return ranges
 }
 
 private fun parseFile(): List<String> =
