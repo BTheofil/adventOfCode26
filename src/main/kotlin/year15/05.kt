330857f2ce7502468af99792651a975662834317
BTheofil <btheofil7@gmail.com>
2026-07-15 18:52:05 +0200

    y15 05 p2

--- src/main/kotlin/year15/05.kt
+++ src/main/kotlin/year15/05.kt
@@ -47,8 +47,39 @@ private fun part1() {
 
 }
 
-private fun part2(){
+private fun part2() {
     val lines = readFileLines("05.txt")
 
+    var counter = 0
+    lines.forEach { line ->
+        val pairs = mutableListOf<String>()
+        var is1ruleCompleted = false
+        var is2ruleCompleted = false
+        for (index in 1..<line.length) {
 
+            //1.rule
+            val currentPair = (line[index - 1]).toString() + line[index].toString()
+
+            if (currentPair !in pairs) {
+                pairs.add(currentPair)
+            } else {
+                if (currentPair != pairs.last()) {
+                    is1ruleCompleted = true
+                }
+            }
+
+            //2.rule
+            if (index >= 2) {
+                if (line[index - 2] == line[index]) {
+                    is2ruleCompleted = true
+                }
+            }
+        }
+
+        if (is1ruleCompleted && is2ruleCompleted) {
+            counter++
+        }
+    }
+
+    println(counter)
 }
