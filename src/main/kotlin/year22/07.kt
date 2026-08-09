e50383629bd2a902bffbfc79c30db8e7ac50c324
BTheofil <btheofil7@gmail.com>
2026-08-09 21:00:34 +0200

    y22 d7 p1&p2

--- src/main/kotlin/year22/07.kt
+++ src/main/kotlin/year22/07.kt
@@ -1,11 +1,65 @@
 package year22
 
-fun main() {
-    part1()
+data class Directory(
+    val name: String,
+    val parent: Directory? = null
+) {
+    val children = mutableMapOf<String, Directory>()
+    var totalSize = 0L
+
+    fun getDirectorySize(): Long {
+        return totalSize + children.values.sumOf { it.getDirectorySize() }
+    }
+
+    fun getAllDirectories(): List<Directory> {
+        return listOf(this) + children.values.flatMap { it.getAllDirectories() }
+    }
 }
 
-private fun part1() {
+fun main() {
+    val root = Directory("/")
+    var currentDirectory = root
+
     readFileLines("07.txt").forEach { line ->
-        println(line)
+        when {
+            line == "$ cd /" -> currentDirectory = root
+            line == "$ cd .." -> currentDirectory = currentDirectory.parent ?: root
+            line.startsWith("$ cd ") -> {
+                val parts = line.split(" ")
+                currentDirectory = currentDirectory.children.getOrPut(
+                    key = parts[2],
+                    defaultValue = {
+                        Directory(name = parts[2], parent = currentDirectory)
+                    }
+                )
+            }
+
+            line == "$ ls" -> {}
+            else -> {
+                val parts = line.split(" ")
+                if (parts[0].toLongOrNull() != null) {
+                    currentDirectory.totalSize += parts[0].toLong()
+                }
+            }
+        }
     }
+
+    val number = root.getAllDirectories()
+        .map { it.getDirectorySize() }
+        .filter { it <= 100_000 }
+        .sum()
+
+    println(number) //part1
+
+    val totalSpace = 70_000_000L
+    val requiredUnused = 30_000_000L
+    val currentUnused = totalSpace - root.getDirectorySize()
+    val neededToFree = requiredUnused - currentUnused
+
+    val number2 = root.getAllDirectories()
+        .map { it.getDirectorySize() }
+        .filter { it >= neededToFree }
+        .minOrNull()
+
+    println(number2)
 }
