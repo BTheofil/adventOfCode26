7c379ec26e7798f0dfaa9405921bc34a3e07602d
BTheofil <btheofil7@gmail.com>
2026-08-15 17:41:14 +0200

    y15 d8 p2

--- src/main/kotlin/year15/08.kt
+++ src/main/kotlin/year15/08.kt
@@ -1,6 +1,11 @@
 package year15
 
 fun main() {
+    part1()
+    part2()
+}
+
+private fun part1() {
     var stringCounter = 0
     var allCounter = 0
 
@@ -16,4 +21,21 @@ fun main() {
     }
 
     println(allCounter - stringCounter)
+}
+
+private fun part2() {
+    var increasedCounter = 0
+    var allCounter = 0
+
+    readFileLines("08.txt").forEach { line ->
+        allCounter += line.count()
+
+        val decoded = line
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+
+        increasedCounter += decoded.length + 2
+    }
+
+    println(increasedCounter - allCounter)
 }
